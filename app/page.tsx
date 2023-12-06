6a4fcb0864cba54c1c02d0e8a229b26d0df3003c
markintela <markintela@hotmail.com>
2023-12-06 17:04:14 +0000

    Hi, world!

---
+++ app/page.tsx
@@ -0,0 +1,95 @@
+import Image from "next/image";
+import ImageLogo from "../public/img/etelvina4.png";
+
+export default function Home() {
+  return (
+    <div className="bg-stone-100">
+      <div className="relative isolate px-6 pt-1 lg:px-8 ">
+        <div
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 "
+          aria-hidden="true"
+        >
+          <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
+        </div>
+        
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 remove-margin">
+          
+          <div className="text-center margin-top">
+          <br />
+          <br />
+           
+            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+              <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                Announcing our next round of funding.{" "}
+                <a href="#" className="font-semibold text-indigo-600">
+                  <span className="absolute inset-0" aria-hidden="true"></span>
+                  Read more <span aria-hidden="true">&rarr;</span>
+                </a>
+              </div>
+            </div>
+            <br />
+            <br />
+          
+
+            <div className="flex justify-center items-center pt-1">
+              <div className="followers_arc_outer items-center followers_arc_start o_circle">
+                <div className="followers_arc_inner items-center followers_arc_start i_circle"></div>
+              </div>
+              <img  id="alien" className=" w-auto " src="./img/etelvina4.png" alt="" />
+            </div>
+            
+            <br />
+            <br />
+            <br />
+            <div className="sm:col-span-2">
+            <div
+              className="flex justify-center items-center pt-1 fala"
+              >
+                <p className="p-1">...</p>
+                  
+             </div>
+             <button id="mic" className="btn btn-lg input talk"><img className=" w-auto  h-12 w-24 " src="./img/mic.svg" alt="" /> </button>        
+              <label
+                htmlFor="message"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Fale comigo!
+              </label>
+              
+              <div className="mt-2.5">
+                <textarea
+                  name="message"
+                  id="response"
+                  rows={5}
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                ></textarea>
+              </div>
+            </div>
+            <br />
+            <br />
+      
+          </div>
+        </div>
+
+        {/* <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)] bg-transparent"
+          aria-hidden="true"
+        >
+          <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"></div>
+        </div> */}
+      </div>
+      <div className="bg-white py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">Trusted by the world’s most innovative teams</h2>
+          <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+            <img className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg" alt="Transistor" width="158" height="48" />
+            <img className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg" alt="Reform" width="158" height="48" />
+            <img className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg" alt="Tuple" width="158" height="48" />
+            <img className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg" alt="SavvyCal" width="158" height="48" />
+            <img className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1" src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg" alt="Statamic" width="158" height="48" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ public/scripts/app.js
@@ -0,0 +1,150 @@
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.fala');
+
+function speak(sentence) {
+    const text_speak = new SpeechSynthesisUtterance(sentence);
+
+    text_speak.rate = 1;
+    text_speak.pitch = 1;
+
+    window.speechSynthesis.speak(text_speak);
+}
+
+function wishMe() {
+    var day = new Date();
+    var hr = day.getHours();
+
+    if(hr >= 0 && hr < 12) {
+        speak("Good Morning Boss");
+    }
+
+    else if(hr == 12) {
+        speak("Good noon Boss");
+    }
+
+    else if(hr > 12 && hr <= 17) {
+        speak("Good Afternoon Boss");
+    }
+
+    else {
+        speak("Good Evening Boss");
+    }
+}
+
+window.addEventListener('load', ()=>{
+    speak("Activating Inertia");
+    speak("Going online");
+    wishMe();
+})
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onresult = (event) => {
+    const current = event.resultIndex;
+    const transcript = event.results[current][0].transcript;
+    content.textContent = transcript;
+   var infos = speakThis(transcript.toLowerCase());
+   if(infos == false){
+    chatGPT(transcript.toLowerCase());
+   }
+
+}
+
+
+const element = document.getElementById('mic');
+element.onclick = function () {recognition.start(); };
+// btn.addEventListener('click', ()=>{
+    
+// })
+
+function speakThis(message) {
+    const speech = new SpeechSynthesisUtterance();
+
+    // speech.text = "Não entedi, pode repetir?";
+
+    if(message.includes('oi') || message.includes('nome')) {
+        var edit_save = document.getElementById("alien");
+        edit_save.src = "./img/etelvina4.png";   
+        const finalText = "Haalloooooo terraqueo! Meu nome é Harley, Você está pronto para uma aventura de outro planeta? Busquem conhecimento!";
+        speech.text = finalText;
+        responseTextarea.value = finalText;
+        
+    }
+
+    else if(message.includes('voar')) {
+        var edit_save = document.getElementById("alien");
+        edit_save.src = "./img/etelvina5.png";   
+        const finalText = "Estou indo passar férias no meu planeta, quer uma carona na minha nave supersonica? ";
+        speech.text = finalText;
+        responseTextarea.value = finalText;
+        
+    }
+
+    var voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[10]; // Note: some voices don't support altering params
+    speech.volume = 1;
+    speech.pitch = 1;
+    speech.rate = 1;
+    if(speech.text != ""){
+        window.speechSynthesis.speak(speech);
+        return true;
+    }
+    return false; 
+}
+
+
+function speakThisGPT(message) {
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = message;
+    
+    var voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[10]; // Note: some voices don't support altering params
+    speech.volume = 1;
+    speech.pitch = 1;
+    speech.rate = 1;
+    window.speechSynthesis.speak(speech); 
+}
+//CHAT GPT SCRIPTS
+
+// const form = document.getElementById('chat-form');
+const mytextInput = document.getElementById('mytext');
+const responseTextarea = document.getElementById('response');
+
+const API_KEY = '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';
+
+async function chatGPT(mytext){
+    if (mytext) {
+        try {
+            const response = await fetch('https://api.openai.com/v1/chat/completions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${API_KEY}`,
+                },
+                body: JSON.stringify({
+                    model: 'gpt-3.5-turbo',
+                    messages: [{ role: 'user', content: "Responda com no maximo 150 caracteres: "+mytext }],
+                    temperature: 1.0,
+                    top_p: 0.7,
+                    n: 1,
+                    stream: false,
+                    presence_penalty: 0,
+                    frequency_penalty: 0,
+                }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                speakThisGPT(data.choices[0].message.content);
+                responseTextarea.value = data.choices[0].message.content;
+            } else {
+                responseTextarea.value = 'Error: Unable to process your request.';
+            }
+        } catch (error) {
+            console.error(error);
+            responseTextarea.value = 'Error: Unable to process your request.';
+        }
+    }
+
+}
